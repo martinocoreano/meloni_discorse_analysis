968e80d3708abbb3b9d2d1e2c67509f68fa29b50
Soohyun Jung <sasha1107@naver.com>
2023-02-01 11:44:39 +0900

    Feat: Progress Bar 추가

--- src/js/script.js
+++ src/js/script.js
@@ -1,21 +1,51 @@
-let pElem = document.createElement("p");
+// let pElem = document.createElement("p");
 
-const response = fetch('./dataset.tsv')
-    .then(response => response.text())
-    .then(v => parseTSV(v))
-    .catch(err => console.log(err))
+// const response = fetch('./dataset.tsv')
+//     .then(response => response.text())
+//     .then(v => parseTSV(v))
+//     .catch(err => console.log(err))
 
-response.then(v => v.forEach(i => i.forEach(item => pElem.innerText += item + "\n")))
+// response.then(v => v.forEach(i => i.forEach(item => pElem.innerText += item + "\n")))
 
-const parseTSV = (data) => {
-    const tsvData = [];
-    const lines = data.split("\n");
+// const parseTSV = (data) => {
+//     const tsvData = [];
+//     const lines = data.split("\n");
 
-    for (let i = 0; i < lines.length; i++) {
-        tsvData[i] = lines[i].split("\t");
-    }
+//     for (let i = 0; i < lines.length; i++) {
+//         tsvData[i] = lines[i].split("\t");
+//     }
     
-    return tsvData;
-};
+//     return tsvData;
+// };
 
-document.querySelector(".text-box").appendChild(pElem);
+// document.querySelector(".text-box").appendChild(pElem);
+
+
+(function (){
+    const barElem = document.querySelector(".progress-bar");
+    const mousePos = { x : 0, y : 0 };
+    let maxScrollValue;
+
+
+    function resizeHandler() {
+        maxScrollValue = document.body.offsetHeight - window.innerHeight;
+    }
+
+    window.addEventListener("scroll", function () {
+        const scrollPer = scrollY / maxScrollValue;
+        const zMove =  scrollPer * 980 - 490;
+        // houseElem.style.transform = `translateZ(${zMove}vw)`;
+        barElem.style.width = `${scrollPer * 100}%`;
+    });
+
+    window.addEventListener("mousemove", function (e) {
+        // 마우스의 위치를 중앙을 0,0으로 하고 상하좌우의 좌표를 -1 ~1 으로 재설정
+        mousePos.x = -1 + (e.clientX / window.innerWidth) * 2;
+        mousePos.y = 1 - (e.clientY / window.innerHeight) * 2;
+        // stageElem.style.transform = `rotateX(${mousePos.y * 5}deg) rotateY(${mousePos.x * 5}deg)`;
+    });
+
+    window.addEventListener("resize", resizeHandler);
+
+    resizeHandler();
+})();
